2372625a3b60e86ec9a960889549e202d95b3598
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-16 20:38:45 -0600

    addItemToCart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -6,8 +6,26 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addItemToCart() {},
-    removeItemFromCart() {},
+    addItemToCart(state, action) {
+      const newItem = action.payload.item;
+      const extistingCartItem = state.items.find(
+        (item) => item.id === newItem.id
+      );
+      if (!extistingCartItem) {
+        state.items.push({
+          ...newItem,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+      } else {
+        extistingCartItem.quantity++;
+        extistingCartItem.totalPrice += newItem.price;
+      }
+    },
+    removeItemFromCart() {
+      const itemId = action.payload.id;
+      const extistingCartItem = state.items.find((item) => item.id === itemId);
+    },
     clearCart() {},
   },
 });
