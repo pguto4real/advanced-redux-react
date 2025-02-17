9c130ead71f4307ee667771ade2eeba795a703c3
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-16 21:16:12 -0600

    update cart button

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,19 @@
-import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from "react-redux";
+
+import { toggle } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+   const cartData = useSelector((state) => state.cart);
+  const toggleCartHandler = () => {
+    dispatch(toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartData.cartTotal}</span>
     </button>
   );
 };
