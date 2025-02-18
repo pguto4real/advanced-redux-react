82e2505f956a772739122c56c4b678d9ba532066
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-17 20:13:37 -0600

    side effect login in app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
+  useEffect(() => {
+    fetch("https://fir-practice-9c832-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {cartIsVisible && <Cart />}
